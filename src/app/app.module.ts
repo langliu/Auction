955c49f03600431ef3f2f490aabd4a0e201eff20
Allen <langliu1216@gmail.com>
2017-05-22 00:13:13 +0800

    引入jquery和bootstrap文件，组件的建立

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { StarsComponent } from './stars/stars.component';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    StarsComponent,
+    ProductComponent,
+    SearchComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
