88bf5c57353e40fe5ae842f2acc722a27015f138
Allen <langliu1216@gmail.com>
2017-05-26 00:04:54 +0800

    添加路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,6 +11,19 @@ import { StarsComponent } from './stars/stars.component';
 import { ProductComponent } from './product/product.component';
 import { SearchComponent } from './search/search.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+
+const routConfig: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'product/:prodTitle',
+    component: ProductDetailComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -19,12 +33,15 @@ import { CarouselComponent } from './carousel/carousel.component';
     StarsComponent,
     ProductComponent,
     SearchComponent,
-    CarouselComponent
+    CarouselComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
