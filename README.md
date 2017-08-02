62536a4ddce0ef5a5bfbd03baa13b9e3e49152b0
Allen <langliu1216@gmail.com>
2017-08-03 00:10:03 +0800

    npm包升级

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Auction
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.2.6.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,16 @@
-import { ProductService } from './shared/product.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { StarsComponent } from './stars/stars.component';
-import { ProductComponent } from './product/product.component';
-import { SearchComponent } from './search/search.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { HomeComponent } from './home/home.component';
-
-const routConfig: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'product/:productId',
-    component: ProductDetailComponent
-  }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    StarsComponent,
-    ProductComponent,
-    SearchComponent,
-    CarouselComponent,
-    ProductDetailComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(routConfig)
+    BrowserModule
   ],
-  providers: [ProductService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
