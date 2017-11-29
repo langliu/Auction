ad83a5c1e7ff14952401d1cfcad5d17feb721d02
Allen <809721414@qq.com>
2017-11-29 21:26:58 +0800

    修改api接口及解决跨域问题

--- server/auction-server.ts
+++ server/auction-server.ts
@@ -11,10 +11,22 @@ const app = express();
  * @type {[Product , Product , Product , Product]}
  */
 const products: Product[] = [
-  new Product(1, '第一个商品', 1.99, 3.4, '这是第一个商品', ['电子产品', '硬件设施']),
-  new Product(2, '第二个商品', 1.99, 4, '这是第二个商品', ['电子产品', '硬件设施']),
-  new Product(3, '第三个商品', 1.99, 5, '这是第三个商品', ['电子产品', '硬件设施']),
-  new Product(4, '第四个商品', 1.99, 1, '这是第四个商品', ['电子产品', '硬件设施'])
+  new Product(1, '第一个商品', 1.99, 3.4, '这是第一个商品', [
+    '电子产品',
+    '硬件设施'
+  ]),
+  new Product(2, '第二个商品', 1.99, 4, '这是第二个商品', [
+    '电子产品',
+    '硬件设施'
+  ]),
+  new Product(3, '第三个商品', 1.99, 5, '这是第三个商品', [
+    '电子产品',
+    '硬件设施'
+  ]),
+  new Product(4, '第四个商品', 1.99, 1, '这是第四个商品', [
+    '电子产品',
+    '硬件设施'
+  ])
 ];
 /**
  * 初始化一些商品的评论信息
@@ -29,6 +41,7 @@ const comments: Comment[] = [
 app.use('/', express.static(path.join(__dirname, '..', 'client')));
 // 获取所有商品
 app.get('/api/products', (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4201');
   let result = products;
   const params = request.query;
   if (params.title) {
@@ -48,18 +61,24 @@ app.get('/api/products', (request, response) => {
 });
 // 根据商品Id获取商品信息
 app.get('/api/product/:id', (request, response) => {
-  response.json(products.find(product => product.id === request.params.id));
-});
-// 根据商品Id获取商品评论
-app.get('/api/product/:id/comments', (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4201');
   response.json(
-    comments.filter(
-      (comment: Comment) => comment.productId === request.params.id
+    products.find(
+      (product: Product) => product.id === Number(request.params.id)
     )
   );
 });
-const server = app.listen(8000, 'localhost', () => {
-  console.log('服务器已启动：http://localhost:8000');
+// 根据商品Id获取商品评论
+app.get('/api/product/:id/comments', (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4201');
+  response.json(
+    comments.filter(
+      (comment: Comment) => comment.productId === Number(request.params.id)
+    )
+  );
+});
+const server = app.listen(4001, 'localhost', () => {
+  console.log('服务器已启动：http://localhost:4001');
 });
 const subscription = new Map<any, number[]>();
 const wsServer = new Server({ port: 8085 });
--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -1,121 +1,133 @@
-import {EventEmitter, Injectable} from '@angular/core';
-import {Http} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 
 @Injectable()
 export class ProductService {
+  public url = 'http://localhost:4001';
+  public searchEvent: EventEmitter<ProductSearchParams> = new EventEmitter();
 
-    searchEvent: EventEmitter<ProductSearchParams> = new EventEmitter();
+  constructor(private http: Http) {}
 
-    constructor(private http: Http) {
-    }
+  /**
+   * 获取所有商品
+   * @returns {Product[]}
+   */
+  getProducts(): Observable<Product[]> {
+    return this.http.get(`${this.url}/api/products`).map(res => res.json());
+  }
 
-    /**
-     * 获取所有商品
-     * @returns {Product[]}
-     */
-    getProducts(): Observable<Product[]> {
-        return this.http.get('/api/products').map(res => res.json());
-    }
+  /**
+   * 根据产品id获取该产品的信息
+   * @param {number} id
+   * @returns {Product}
+   */
+  getProduct(id: number): Observable<Product> {
+    return this.http
+      .get(`${this.url}/api/product/` + id)
+      .map(res => res.json());
+  }
 
-    /**
-     * 根据产品id获取该产品的信息
-     * @param {number} id
-     * @returns {Product}
-     */
-    getProduct(id: number): Observable<Product> {
-        return this.http.get('/api/product/' + id).map(res => res.json());
-    }
+  /**
+   * 根据产品id获取该产品的评论信息
+   * @param {number} id - 需要获取评论信息的产品的产品id
+   * @returns {Observable<Comment[]>} - 该Id对应的评论的数组
+   */
+  getCommentsForProductId(id: number): Observable<Comment[]> {
+    return this.http
+      .get(`${this.url}/api/product/${id}/comments`)
+      .map(res => res.json());
+  }
 
-    /**
-     * 根据产品id获取该产品的评论信息
-     * @param {number} id - 需要获取评论信息的产品的产品id
-     * @returns {Observable<Comment[]>} - 该Id对应的评论的数组
-     */
-    getCommentsForProductId(id: number): Observable<Comment[]> {
-        return this.http.get(`/api/product/${id}/comments`).map(res => res.json());
-    }
+  /**
+   * 获取所有的商品的分类
+   * @returns {string[]} - 商品分类名称
+   */
+  getAllCategories(): string[] {
+    return ['电子产品', '硬件设施', '图书'];
+  }
 
-    /**
-     * 获取所有的商品的分类
-     * @returns {string[]} - 商品分类名称
-     */
-    getAllCategories(): string[] {
-        return ['电子产品', '硬件设施', '图书'];
-    }
+  /**
+   * 商品搜索
+   * @param {ProductSearchParams} params - 搜索参数
+   * @returns {Observable<Product[]>} - 符合条件的商品
+   */
+  search(params: ProductSearchParams): Observable<Product[]> {
+    return this.http
+      .get(`${this.url}/api/products`, { search: this.encodeParams(params) })
+      .map(res => res.json());
+  }
 
-    /**
-     * 商品搜索
-     * @param {ProductSearchParams} params - 搜索参数
-     * @returns {Observable<Product[]>} - 符合条件的商品
-     */
-    search(params: ProductSearchParams): Observable<Product[]> {
-        return this.http.get('/api/products', {search: this.encodeParams(params)}).map(res => res.json());
-    }
-
-    /**
-     * 搜索参数处理
-     * @param {ProductSearchParams} params - 转入的搜索数据
-     * @returns {string} - 返回一个字符串
-     */
-    encodeParams(params: ProductSearchParams) {
-        return Object.keys(params).filter(key => params[key]).reduce((param: string, key: string) => {
-            if (param) {
-                param += `&&${key}=${params[key]}`;
-            } else {
-                param += `${key}=${params[key]}`;
-            }
-            return param;
-        }, '');
-    }
+  /**
+   * 搜索参数处理
+   * @param {ProductSearchParams} params - 转入的搜索数据
+   * @returns {string} - 返回一个字符串
+   */
+  encodeParams(params: ProductSearchParams) {
+    return Object.keys(params)
+      .filter(key => params[key])
+      .reduce((param: string, key: string) => {
+        if (param) {
+          param += `&&${key}=${params[key]}`;
+        } else {
+          param += `${key}=${params[key]}`;
+        }
+        return param;
+      }, '');
+  }
 }
 
 export class Product {
-    /**
-     * Product类的构造函数属性
-     * @param {number} id - 商品编号
-     * @param {string} title - 商品名称
-     * @param {number} price - 商品单价
-     * @param {number} rating - 商品星级
-     * @param {string} desc - 商品描述
-     * @param {string[]} categories - 商品所属类别
-     */
-    constructor(public id: number,
-                public title: string,
-                public price: number,
-                public rating: number,
-                public desc: string,
-                public categories: string[]) {
-    }
+  /**
+   * Product类的构造函数属性
+   * @param {number} id - 商品编号
+   * @param {string} title - 商品名称
+   * @param {number} price - 商品单价
+   * @param {number} rating - 商品星级
+   * @param {string} desc - 商品描述
+   * @param {string[]} categories - 商品所属类别
+   */
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: string[]
+  ) {}
 }
 
 export class Comment {
-    /**
-     * Comment类的构造函数属性
-     * @param {number} id - 评论的id
-     * @param {number} productId - 评论的产品的id
-     * @param {string} timestamp - 评论的时间
-     * @param {string} user - 发表评论的人
-     * @param {number} rating - 发表评论的人对该产品的评星
-     * @param {string} content - 评论的内容
-     */
-    constructor(public id: number,
-                public productId: number,
-                public timestamp: string,
-                public user: string,
-                public rating: number,
-                public content: string) {
-    }
+  /**
+   * Comment类的构造函数属性
+   * @param {number} id - 评论的id
+   * @param {number} productId - 评论的产品的id
+   * @param {string} timestamp - 评论的时间
+   * @param {string} user - 发表评论的人
+   * @param {number} rating - 发表评论的人对该产品的评星
+   * @param {string} content - 评论的内容
+   */
+  constructor(
+    public id: number,
+    public productId: number,
+    public timestamp: string,
+    public user: string,
+    public rating: number,
+    public content: string
+  ) {}
 }
 
 export class ProductSearchParams {
-    /**
-     *
-     * @param {string} title - 搜索的商品的名称
-     * @param {number} price - 搜索的价格
-     * @param {string} category - 搜索的商品所属的商品分类
-     */
-    constructor(public title: string, public price: number, public category: string) {
-    }
+  /**
+   *
+   * @param {string} title - 搜索的商品的名称
+   * @param {number} price - 搜索的价格
+   * @param {string} category - 搜索的商品所属的商品分类
+   */
+  constructor(
+    public title: string,
+    public price: number,
+    public category: string
+  ) {}
 }
